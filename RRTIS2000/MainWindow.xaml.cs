adf2e5582b8532f9f3ae5ced2b5ffa34f67aed45
Gankov Andrey <gav@qsolution.ru>
2015-12-28 16:58:04 +0300

    Save and Extract

--- RRTIS2000/MainWindow.xaml.cs
+++ RRTIS2000/MainWindow.xaml.cs
@@ -36,35 +36,43 @@ namespace RRTIS2000
 
 		private void Button_Click(object sender, RoutedEventArgs e)
 		{
-			using (OdbcCommand com = new OdbcCommand(
-				"SELECT * FROM Calibration_Files WHERE Calibration_Part_No = ?", Connection))
+			try
 			{
-				com.Parameters.AddWithValue("@var", tbNumber.Text);
 
-				using (OdbcDataReader reader = com.ExecuteReader())
+				using (OdbcCommand com = new OdbcCommand(
+					"SELECT * FROM Calibration_Files WHERE Calibration_Part_No = ?", Connection))
 				{
-					if (!reader.HasRows)
+					com.Parameters.AddWithValue("@var", tbNumber.Text);
+
+					using (OdbcDataReader reader = com.ExecuteReader())
 					{
-						LableResult.Content = "Указанный номер не найден в БД.";
-						return;
+						if (!reader.HasRows)
+						{
+							LableResult.Content = "Указанный номер не найден в БД.";
+							return;
+						}
+						reader.Read();
+
+						ReadedFile = (byte[])reader.GetValue(1);
+						number = tbNumber.Text;
+
+						LableResult.Content = String.Format("Загружен файл gzip, размером {0} байт.", ReadedFile.Length);
+						btSave.IsEnabled = btSave_Extract.IsEnabled = true;
 					}
-					reader.Read();
-
-					ReadedFile = (byte[])reader.GetValue(1);
-					number = tbNumber.Text;
-
-					LableResult.Content = String.Format("Загружен файл {0} байт.", ReadedFile.Length);
-					btSave.IsEnabled = true;
 				}
 			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(String.Format("Ошибка при запросе: {0}", ex.ToString()));
+			}
+
 		}
 
 		private void btSave_Click(object sender, RoutedEventArgs e)
 		{
 			SaveFileDialog dlg = new SaveFileDialog();
-			dlg.FileName = String.Format("{0}.bin", number); // Default file name
-			dlg.DefaultExt = ".bin"; // Default file extension
-			//dlg.Filter = "Text documents (.txt)|*.txt"; // Filter files by extension
+			dlg.FileName = String.Format("{0}.gzip", number); // Default file name
+			dlg.DefaultExt = ".gzip"; // Default file extension
 
 			// Show save file dialog box
 			Nullable<bool> result = dlg.ShowDialog();
@@ -105,7 +113,7 @@ namespace RRTIS2000
 
 			if (list.Length > 0)
 			{
-				SaveFileDialog dlg = new SaveFileDialog();
+				Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
 				dlg.FileName = String.Format("allNumbers.txt"); // Default file name
 				dlg.Filter = "Text documents (.txt)|*.txt"; // Filter files by extension
 
@@ -130,6 +138,37 @@ namespace RRTIS2000
 					System.Diagnostics.Process.Start(dlg.FileName);
 				}
 			}
+		}
+
+		private void btSave_Extract_Click(object sender, RoutedEventArgs e)
+		{
+			System.Windows.Forms.FolderBrowserDialog fbd = new System.Windows.Forms.FolderBrowserDialog();
+			fbd.Description = "Выберите папку для распаковки";
+			var result = fbd.ShowDialog();
+
+			// Process save file dialog box results
+			if (result == System.Windows.Forms.DialogResult.OK)
+			{
+				// Save document
+				try
+				{
+					var tempDir = System.IO.Path.GetTempPath();
+					var gzipFile = System.IO.Path.Combine(tempDir, String.Format("{0}.gzip", number));
+					File.WriteAllBytes(gzipFile, ReadedFile);
+
+					string command = "7z.exe";
+					string arguments = String.Format("e -o{0} {1}", fbd.SelectedPath, gzipFile);
+
+					System.Diagnostics.Process.Start(command, arguments);
+				}
+				catch (Exception _Exception)
+				{
+					// Error
+					MessageBox.Show(String.Format("Exception caught in process: {0}", _Exception.ToString()));
+				}
+
+			}
+
 		}
 	}
 }
